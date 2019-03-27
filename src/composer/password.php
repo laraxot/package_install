71fcaca5ca4da43bf5b70e054b92c5abb58c44e9
marco76tv <marco.sottana@gmail.com>
2019-03-27 13:06:02 +0100

    .

--- src/composer/password.php
+++ src/composer/password.php
@@ -1,6 +1,5 @@
 <?php
 
-
 $password = 'password1234';
 if (!isset($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_PW'] !== $password) {
     \header('WWW-Authenticate: Basic realm="NoConsoleComposer"');
