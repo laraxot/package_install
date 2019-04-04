d3dcfe0e2081ac09354a96b6eb5b6b38cb4dbdd9
marco xot <marco.sottana@gmail.com>
2019-04-04 19:24:25 +0200

    baseServiceProvider

--- src/InstallServiceProvider.php
+++ src/InstallServiceProvider.php
@@ -1,17 +1,14 @@
 <?php
-
-
-
 namespace XRA\Install;
+use XRA\Extend\BaseServiceProvider;
+//use Illuminate\Support\ServiceProvider;
+//use XRA\Extend\Traits\ServiceProviderTrait;
 
-use Illuminate\Support\ServiceProvider;
-use XRA\Extend\Traits\ServiceProviderTrait;
-
-class InstallServiceProvider extends ServiceProvider
+class InstallServiceProvider extends BaseServiceProvider
 {
-    use ServiceProviderTrait;
+    //use ServiceProviderTrait;
 
-    protected $defer = true;
+    //protected $defer = true;
 
     /**
      * Register the application services.
